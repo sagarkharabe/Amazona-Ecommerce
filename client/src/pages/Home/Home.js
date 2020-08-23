4335c38d32c0cd13530653b37c8807e3eca6f869
sagarkharbe <sagarkharabe@yahoo.in>
2020-08-24 01:35:19 +0530

    add filters

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,30 +1,68 @@
-import React, { useEffect } from 'react';
-import { Typography, } from 'antd';
+import React, { useEffect, useState, useMemo } from 'react';
+import { Typography, Input, Select } from 'antd';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import Cart from '../../components/Cart/Cart';
 import { getProducts } from '../../store/actions/productsActions';
 import ProductCard from '../../components/Product/Product';
+import { categories } from '../../constants/categories';
+import { filter } from '../../util/filterProducts';
 
 import Layout from '../../layout/Layout';
 
 import './styles.css';
 
-const Home = ({ auth, products: { products }, getProducts, cartItems }) => {
+const { Option } = Select;
+const { Search } = Input;
+
+const Home = ({ products: { products }, getProducts, cartItems }) => {
   useEffect(() => {
     getProducts();
   }, []);
+
+  const [categoryFilter, setCategoryFilter] = useState('');
+  const [nameFilter, setNameFilter] = useState('');
+  const [priceFilter, setPriceFilter] = useState('');
+
+  const filteredProducts = useMemo(
+    () => filter(products, categoryFilter, nameFilter, priceFilter),
+    [categoryFilter, nameFilter, priceFilter],
+  );
 
   return (
     <Layout>
       <div className="home-page">
         <Typography.Title level={2}>Amazona E-Commerce</Typography.Title>
-        <Typography.Title level={4} >Buy and Sell globally.</Typography.Title>
+        <Typography.Title level={4}>Buy and Sell globally.</Typography.Title>
         <div>
-          <Typography.Text strong >Welcome {auth.isAuthenticated ? `${auth.me.name}` : 'guest'}! </Typography.Text>
-          <p></p>
+          <Typography.Text strong>Filter By:</Typography.Text>
+
+          <div style={{ display: 'flex' }}>
+            <Select
+              defaultValue="Select a Category"
+              value={categoryFilter}
+              onChange={setCategoryFilter}
+              style={{ marginRight: 20 }}
+            >
+              <Option value={''}>Select a Category</Option>
+              {categories.map((x) => (
+                <Option value={x}>{x}</Option>
+              ))}
+            </Select>
+            <Select
+              defaultValue="Select a Category"
+              value={priceFilter}
+              onChange={setPriceFilter}
+              style={{ marginRight: 20 }}
+            >
+              <Option value={''}>Set Price Filter</Option>
+              <Option value={'asc'}>Lowest to Highest</Option>
+              <Option value={'desc'}>Highest to Lowest</Option>
+            </Select>
+            <Search placeholder="Search by name" onSearch={setNameFilter} style={{ width: 200 }} />
+          </div>
           <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-            {products.map((product) => (
+            {filteredProducts.map((product) => (
               <ProductCard item={product} />
             ))}
           </div>
@@ -36,7 +74,6 @@ const Home = ({ auth, products: { products }, getProducts, cartItems }) => {
 };
 
 const mapStateToProps = (state) => ({
-  auth: state.auth,
   cartItems: state.cart.cartItems,
   products: state.products,
 });
