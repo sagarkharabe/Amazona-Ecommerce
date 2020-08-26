b13ab84b53568a26731c0e1b41c5a481e38aa500
sagarkharbe <sagarkharabe97@gmail.com>
2020-08-26 20:54:25 +0530

    added cart

--- client/src/components/Notification/Notification.js
+++ client/src/components/Notification/Notification.js
@@ -4,5 +4,6 @@ export const openNotificationWithIcon = ({ type, message, description }) => {
   notification[type]({
     message: message,
     description: description,
+    placement: 'bottomRight',
   });
 };
